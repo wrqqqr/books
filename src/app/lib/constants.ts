ad23e651211cd64357961cc1573e6e8d83a03e91
Ilya <wrqqqrrr@gmail.com>
2024-06-26 19:11:36 +0300

    Add card function to render each book, add lodash and something else

---
+++ src/app/lib/constants.ts
@@ -0,0 +1,4 @@
+const MAX_RESULTS: number = 6;
+const ITEMS_PER_PAGE: number = 12;
+
+export { MAX_RESULTS, ITEMS_PER_PAGE };
--- src/app/ui/AppBar/AppBar.tsx
+++ src/app/ui/AppBar/AppBar.tsx
@@ -1,15 +1,26 @@
 "use client";
 
-import * as React from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import axios from "axios";
+import {
+  TextField,
+  InputBase,
+  Toolbar,
+  Box,
+  AppBar,
+  Grid,
+  Typography,
+  Pagination,
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+} from "@mui/material";
 import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
+import debounce from "lodash.debounce";
+
+const defaultImage = "./default-image.jpg";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -54,8 +65,45 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function SearchAppBar() {
+  const [query, setQuery] = useState("Lovecraft");
+  const [books, setBooks] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const fetchBooks = useCallback(async (query: string, startIndex = 0) => {
+    if (query.length === 0) {
+      setBooks([]);
+      return;
+    }
+    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_KEY;
+    let response = null;
+    try {
+      response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${startIndex}&maxResults=6&key=${apiKey}`,
+      );
+    } catch (error) {
+      console.error("Ошибка при запросе книг:", error);
+    }
+    setBooks(response?.data.items || []);
+    setTotalPages(Math.ceil(response?.data.totalItems / 12));
+  }, []);
+
+  const debouncedFetchBooks = useCallback(
+    debounce((q) => fetchBooks(q), 500),
+    [fetchBooks],
+  );
+
+  useEffect(() => {
+    debouncedFetchBooks(query);
+  }, [query, debouncedFetchBooks]);
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+    fetchBooks(query, (value - 1) * 12);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Container>
       <AppBar position="static">
         <Toolbar>
           <Typography
@@ -73,10 +121,79 @@ export default function SearchAppBar() {
             <StyledInputBase
               placeholder="Search…"
               inputProps={{ "aria-label": "search" }}
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
             />
           </Search>
         </Toolbar>
       </AppBar>
-    </Box>
+      <Grid container spacing={2} marginTop={2}>
+        {books.map((book) => {
+          const { title, authors, description, imageLinks } = book.volumeInfo;
+          return (
+            <Grid item xs={12} sm={6} md={4} key={book.id}>
+              <Card>
+                {imageLinks && imageLinks.thumbnail && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: 200,
+                      overflow: "hidden",
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        height: "100%",
+                        width: "auto",
+                        objectFit: "contain",
+                      }}
+                      alt={title}
+                      image={imageLinks?.thumbnail || defaultImage}
+                      title={title}
+                      onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.src = defaultImage;
+                      }}
+                    />
+                  </Box>
+                )}
+                <CardContent>
+                  <Typography variant="h6" component="div">
+                    {title}
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="textSecondary"
+                    component="div"
+                  >
+                    {authors?.join(", ")}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {description?.substring(0, 100)}...
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+      {totalPages > 1 && (
+        <Box display="flex" justifyContent="center" marginTop={2}>
+          <Pagination
+            count={totalPages}
+            page={currentPage}
+            onChange={handlePageChange}
+            color="primary"
+          />
+        </Box>
+      )}
+    </Container>
   );
 }
